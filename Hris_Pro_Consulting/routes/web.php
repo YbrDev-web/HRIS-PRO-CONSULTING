b17bf3d1bb23f5cfdbe58666f2d5c44f7c3f1eca
YbrDev-web <ybenromdhane2003@gmail.com>
2025-04-28 14:38:16 +0200

    route modifié

--- Hris_Pro_Consulting/routes/web.php
+++ Hris_Pro_Consulting/routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+use Illuminate\http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hris_pro_consulting', function () {
-    return 'bonjour';
+Route::get('/hris_pro_consulting', function (Request $request) {
+    return [
+        "name"=> $request->input('name', 'john Doe'),
+        "article" => "article 1"
+    ];
 });
 
