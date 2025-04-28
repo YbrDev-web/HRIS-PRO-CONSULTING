b90a8fa84edf7e372fb292ee9c96fdb2c9a678f1
YbrDev-web <ybenromdhane2003@gmail.com>
2025-04-28 14:29:21 +0200

    routes

--- Hris_Pro_Consulting/routes/web.php
+++ Hris_Pro_Consulting/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hris_pro_consulting', function () {
+    return 'bonjour';
+});
+
